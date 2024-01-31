4540f692beaab4792f108e53c1b07720d8adabc0
Vincent Bénet <vincent.benet@outlook.fr>
2024-01-31 15:46:21 +0100

    working_keyboard

--- code/Core/Inc/main.h
+++ code/Core/Inc/main.h
@@ -7,7 +7,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2023 STMicroelectronics.
+  * Copyright (c) 2024 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -57,36 +57,6 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define C10_Pin GPIO_PIN_2
-#define C10_GPIO_Port GPIOA
-#define C9_Pin GPIO_PIN_3
-#define C9_GPIO_Port GPIOA
-#define C8_Pin GPIO_PIN_4
-#define C8_GPIO_Port GPIOA
-#define C7_Pin GPIO_PIN_5
-#define C7_GPIO_Port GPIOA
-#define C6_Pin GPIO_PIN_6
-#define C6_GPIO_Port GPIOA
-#define C5_Pin GPIO_PIN_7
-#define C5_GPIO_Port GPIOA
-#define C4_Pin GPIO_PIN_0
-#define C4_GPIO_Port GPIOB
-#define L2_Pin GPIO_PIN_1
-#define L2_GPIO_Port GPIOB
-#define L1_Pin GPIO_PIN_2
-#define L1_GPIO_Port GPIOB
-#define C3_Pin GPIO_PIN_8
-#define C3_GPIO_Port GPIOA
-#define C2_Pin GPIO_PIN_9
-#define C2_GPIO_Port GPIOA
-#define C1_Pin GPIO_PIN_10
-#define C1_GPIO_Port GPIOA
-#define C11_Pin GPIO_PIN_5
-#define C11_GPIO_Port GPIOB
-#define L4_Pin GPIO_PIN_6
-#define L4_GPIO_Port GPIOB
-#define L3_Pin GPIO_PIN_7
-#define L3_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- code/Debug/Core/Src/main.d
+++ code/Debug/Core/Src/main.d
@@ -30,7 +30,13 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pcd_ex.h \
  ../USB_DEVICE/App/usb_device.h \
  ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
- ../USB_DEVICE/Target/usbd_conf.h
+ ../USB_DEVICE/Target/usbd_conf.h \
+ ../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc/usbd_hid.h \
+ ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
 ../Core/Inc/main.h:
 ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
 ../Core/Inc/stm32l0xx_hal_conf.h:
@@ -64,3 +70,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../USB_DEVICE/App/usb_device.h:
 ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
 ../USB_DEVICE/Target/usbd_conf.h:
+../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc/usbd_hid.h:
+../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
